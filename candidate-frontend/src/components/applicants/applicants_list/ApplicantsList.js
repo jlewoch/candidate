b062e14338dfa33cc135ffc5c749ca6826497709
Jake Lewochko <jlewochko@gmail.com>
2018-11-12 09:50:43 -0500

    adding applicants section part redesigned restructured folders

---
+++ candidate-frontend/src/components/applicants/applicants_list/ApplicantsList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ApplicantsList = () => {
+  return <div />
+}
+
+export default ApplicantsList
--- candidate-frontend/src/main/Main.js
+++ candidate-frontend/src/main/Main.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
 import styled from 'styled-components'
-
 import ApplicantCard
   from '../components/applicants/applicant_card/ApplicantCard'
-import ApplicantProgress
-  from '../components/applicants/applicant_progress/ApplicantProgress'
+import { Applicants } from '../components/applicants'
+
 const Main = () => {
   const MainDiv = styled.div`
 
     `
   return (
     <MainDiv>
-      <ApplicantProgress />
+      <Applicants />
     </MainDiv>
   )
 }
