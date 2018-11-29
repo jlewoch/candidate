0178a3c9ecc99dab5c279345b7c50991565ac245
Jake Lewochko <jlewochko@gmail.com>
2018-11-29 15:25:00 -0500

    starting to restructure backend

--- candidate-backend/knexfile.js
+++ candidate-backend/knexfile.js
@@ -1,16 +1,21 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'mysql2',
-    connection: {
-        host: 'localhost',
-        port:'3306',
-        user: 'root',
-        password: '@dobe;Flash',
-        database: 'Candidate'
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    }
+  },
+
+  ssl: true,
   pool: { min: 0, max: 7 }
-};
+}
